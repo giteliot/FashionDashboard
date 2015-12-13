9577b11e1b4e1892f7725cbd655c678165af7466
ELIOT <eliachinellato@gmail.com>
2015-12-13 22:51:47 +0100

    Login Page

--- FashionDashboard/WebContent/js/index.js
+++ FashionDashboard/WebContent/js/index.js
@@ -6,23 +6,32 @@ function changePage(page) {
 	
 	if (page == "loginPage") {
 		$(".header-btn").hide();
+		$("#fbLogin").show();
+		$('.content').css('background-image',"url('images/background.jpg')");
+		$("titleDash").html("Burton Analytics");
 	} else if (page == "dashPage") {
 		$(".header-btn").show();
 		$(".header-btn").html("Logout");
 		$(".header-btn").attr("onclick","doLogout()");
+		$("#fbLogin").hide();
+		$('.content').css('background-image','none');
 	} else {
 		$(".header-btn").show();
 		$(".header-btn").html("Home");
 		$(".header-btn").attr("onclick","doLogin()");
+		$("#fbLogin").hide();
+		$('.content').css('background-image','none');
 	}
 		
 }
 
 $(document).ready(function(){	
 	changePage("loginPage");
+	$(".content").css('background-size',""+window.width+"px "+window.height+"px");
 });
 
-function doLogin() {
+function doLogin(username) {
+	$('#titleDash').html(username+"'s Burton Analytics");
 	changePage("dashPage");
 }
 
